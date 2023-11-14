1eff6528a7834cab77e675407b4e9532fc42ba0b
Nathaniel Carlo <100833190+xynoan@users.noreply.github.com>
2023-11-14 22:08:02 +0800

    refine code

--- main.cpp
+++ main.cpp
@@ -4,22 +4,20 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-const string accounts_file = "accounts.txt";
+std::string accounts_file = "accounts.txt";
 
 struct Account {
-  string first_name = "";
-  string last_name = "";
-  string password = "";
-  string email = "";
+  std::string first_name = "";
+  std::string last_name = "";
+  std::string password = "";
+  std::string email = "";
   bool equals(Account to_compare) { return email == to_compare.email; }
 };
 
-vector<Account> read_accounts_from_file() {
-  vector<Account> accounts;
+std::vector<Account> read_accounts_from_file() {
+  std::vector<Account> accounts;
   Account account;
-  ifstream input_file(accounts_file);
+  std::ifstream input_file(accounts_file);
   if (input_file) {
     while (input_file >> account.first_name >> account.last_name >>
            account.password >> account.email) {
@@ -30,18 +28,19 @@ vector<Account> read_accounts_from_file() {
   return accounts;
 }
 
-void display_accounts(vector<Account> accounts) {
+void display_accounts(std::vector<Account> accounts) {
   int col_width = 10;
-  cout << left << setw(col_width * 3) << "Name" << setw(col_width * 4)
-       << "Email" << endl;
+  std::cout << std::left << std::setw(col_width * 3) << "Name"
+            << std::setw(col_width * 4) << "Email" << '\n';
   for (Account account : accounts) {
-    cout << setw(col_width * 3) << account.first_name + ' ' + account.last_name
-         << setw(col_width * 4) << account.email << endl;
+    std::cout << std::setw(col_width * 3)
+              << account.first_name + ' ' + account.last_name
+              << std::setw(col_width * 4) << account.email << '\n';
   }
 }
 
-void write_accounts_to_file(vector<Account> accounts) {
-  ofstream output_file(accounts_file);
+void write_accounts_to_file(std::vector<Account> accounts) {
+  std::ofstream output_file(accounts_file);
   if (output_file) {
     for (Account account : accounts) {
       output_file << account.first_name << '\t' << account.last_name << '\t'
@@ -53,21 +52,21 @@ void write_accounts_to_file(vector<Account> accounts) {
 
 Account get_account() {
   Account account;
-  cout << "First name: ";
-  getline(cin, account.first_name);
-  cout << "Last name: ";
-  getline(cin, account.last_name);
-  cout << "Password: ";
-  getline(cin, account.password);
-  cout << "Email: ";
-  getline(cin, account.email);
+  std::cout << "First name: ";
+  getline(std::cin, account.first_name);
+  std::cout << "Last name: ";
+  getline(std::cin, account.last_name);
+  std::cout << "Password: ";
+  getline(std::cin, account.password);
+  std::cout << "Email: ";
+  getline(std::cin, account.email);
 
   return account;
 }
 
 int main() {
-  vector<Account> accounts = read_accounts_from_file();
-  cout << "Create Account List\n\n";
+  std::vector<Account> accounts = read_accounts_from_file();
+  std::cout << "Create Account List\n\n";
 
   display_accounts(accounts);
 
@@ -77,7 +76,7 @@ int main() {
     bool alreadyExist = false;
     for (Account existingAccs : accounts) {
       if (existingAccs.equals(account)) {
-        cout << "\nThat account already exist!\n";
+        std::cout << "\nThat account already exist!\n";
         alreadyExist = true;
         break;
       }
@@ -89,15 +88,15 @@ int main() {
 
     accounts.push_back(account);
     write_accounts_to_file(accounts);
-    cout << endl
-         << account.email << " was added for "
-         << account.first_name + ' ' + account.last_name << endl
-         << endl;
+    std::cout << '\n'
+              << account.email << " was added for "
+              << account.first_name + ' ' + account.last_name << '\n'
+              << '\n';
 
-    cout << "Enter another account? (y/n): ";
-    cin >> another;
-    cin.ignore();
-    cout << endl;
+    std::cout << "Enter another account? (y/n): ";
+    std::cin >> another;
+    std::cin.ignore();
+    std::cout << '\n';
   }
 
   display_accounts(accounts);
